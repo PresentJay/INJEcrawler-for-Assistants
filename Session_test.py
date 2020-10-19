2c76f0eaabda864fb4a0b089ee5803d40d2aa634
PresentJay <presentj94@gmail.com>
2020-10-19 12:24:06 +0900

    feat : correction of paginated scraping, add retry access session

--- Session_test.py
+++ Session_test.py
@@ -25,24 +25,33 @@ def test(url, ID_, PW_, target_url_prev, target_url_next):
             raise Exception(
                 'could not login. please check your ID or PW again.') """
 
+        # pagenated scraping
         pg_count = 1
         target_list = []
         while(1):
             try:
                 res = s.get(target_url_prev + str(pg_count) + target_url_next)
                 soup = BeautifulSoup(res.text, 'html.parser')
-                if soup.select_one('#kboard-default-list > div.kboard-pagination > ul > li.last-page') != None:
-                    tmp_list = soup.select(
-                        '#kboard-default-list > div.kboard-list > table > tbody > tr > td.kboard-list-title > a')
+                tmp_list = soup.select(
+                    '#kboard-default-list > div.kboard-list > table > tbody > tr > td.kboard-list-title > a')
+
+                if tmp_list != None:
                     for item in tmp_list:
                         target_list.append(item.get('href'))
                         print('page', pg_count, 'item', len(
                             target_list), 'is in progress')
-                else:
+
+                if soup.select_one('#kboard-default-list > div.kboard-pagination > ul > li.last-page') is None:
+                    print('paginated scraping done.')
                     break
 
                 pg_count += 1
-            except:
-                print('error raised')
 
-        print(len(target_list), ':', target_list)
+            # session retry when session is aborted with connection error or other reason
+            except:
+                print('error raised in page', pg_count, 'try to retry . . .')
+                res = s.post(url, data=login_info)
+                res.raise_for_status()
+
+        for item in target_list:
+            print(item)
